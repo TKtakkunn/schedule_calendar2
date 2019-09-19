8d670f41bd404e90fb8c52c6a30c04c3e63cbcf6
TakumiKatase <>
2019-09-19 16:04:42 +0900

    カラムend_timeの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_14_071107) do
+ActiveRecord::Schema.define(version: 2019_09_19_065930) do
 
   create_table "meetings", force: :cascade do |t|
     t.string "name"
     t.datetime "start_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "end_time"
   end
 
 end
